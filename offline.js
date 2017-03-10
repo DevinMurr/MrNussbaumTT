b68398cb490a42a19e65a86615213e5eed923e36
DevinMurr <djmurray92@gmail.com>
2017-03-10 16:25:22 -0500

    Merge remote-tracking branch 'origin/gh-pages' into gh-pages
    # Conflicts:
    #	c2runtime.js
    #	data.js
    #	images/obj_enemy-sheet0.png
    #	images/obj_tank-sheet0.png
    #	offline.appcache
    #	offline.js
    
    combat in

--- offline.js
+++ offline.js
@@ -1,19 +1,47 @@
 ﻿{
-	"version": 1488220689,
+<<<<<<< HEAD
+	"version": 1489180972,
+=======
+	"version": 1488921008,
+>>>>>>> origin/gh-pages
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"images/obj_tank-sheet0.png",
+		"images/obj_tank-sheet1.png",
+		"images/obj_tank-sheet2.png",
 		"images/obj-sheet0.png",
+		"images/obj-sheet1.png",
 		"images/obj_joystickbg-sheet0.png",
 		"images/obj_joystickinnercircle-sheet0.png",
 		"images/obj_arrow-sheet0.png",
+		"images/obj_enemy-sheet0.png",
+		"images/obj_waypoint-sheet0.png",
+		"images/obj_targetreticle-sheet0.png",
+		"images/obj_sanfranroad-sheet0.png",
+<<<<<<< HEAD
+		"images/obj_sanfranroad-sheet1.png",
+		"images/obj_sanfrancityscape-sheet0.png",
+		"images/obj_sanfrancityscape-sheet1.png",
+		"images/obj_sanfranbuildings-sheet0.png",
+		"images/obj_sanfranbuildings-sheet1.png",
+		"images/obj_sanfranlandingpad-sheet0.png",
+		"images/obj_buttonbackground-sheet0.png",
+		"images/obj_bullet-sheet0.png",
+		"images/obj_chicagolandingpads-sheet0.png",
+		"images/obj_vowelbutton-sheet0.png",
+=======
+		"images/obj_sanfrancityscape-sheet0.png",
+		"images/obj_sanfranbuildings-sheet0.png",
+		"images/obj_sanfranlandingpad-sheet0.png",
+>>>>>>> origin/gh-pages
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"pathfind.js"
 	]
 }
